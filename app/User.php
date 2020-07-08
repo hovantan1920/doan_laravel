32bf000ef4d3c40e08f3bdc7a4df729139c3cd41
minhminh <minhminh9201@gmail.com>
2020-07-08 07:17:46 +0700

    User birthday...

--- app/User.php
+++ app/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'roles_id'
+        'name', 'email', 'password', 'roles_id', 'gender', 'birthday', 'numberphone'
     ];
 
     /**
